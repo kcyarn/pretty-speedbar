19f42dd9d12ba672be1e82542b9d426e6c269422
Kristle Chester <kcyarn7@gmail.com>
2022-01-17 16:15:45 -0500

    Removed unused icons and added documentation.
    Removed the leftover icon defcustoms from the swap to optional plus/minus in the svg generation. Added documentation for each new defezimage. Completed adding pretty-speedbar's icons to the speedbar image alist.

--- pretty-speedbar.el
+++ pretty-speedbar.el
@@ -67,12 +67,12 @@
   :type '(string)
   :group 'pretty-speedbar)
 
-(defcustom pretty-speedbar-check-fill "#FF0000"
+(defcustom pretty-speedbar-about-fill "#FF0000"
   "Set default non-folder fill color as hex."
   :type '(string)
   :group 'pretty-speedbar)
 
-(defcustom pretty-speedbar-check-stroke "#333333"
+(defcustom pretty-speedbar-about-stroke "#333333"
   "Set default non-folder stroke color as hex."
   :type '(string)
   :group 'pretty-speedbar)
@@ -99,20 +99,12 @@ Defaults to off."
   "Lock icon from FontAwesome used by pretty-lock."
   :group 'pretty-speedbar)
 
-(defcustom pretty-speedbar-chevron-down '("\uf13a" nil)
-  "Chevron-circle-down from FontAwesome used by pretty-chevron-down."
-  :group 'pretty-speedbar)
-
-(defcustom pretty-speedbar-chevron-right '("\uf138" nil)
-  "Chevron-circle-right from FontAwesome used by pretty-chevron-right."
-  :group 'pretty-speedbar)
-
 (defcustom pretty-speedbar-tag '("\uf02b" nil)
   "Tag from FontAwesome used by pretty-tag."
   :group 'pretty-speedbar)
 
 (defcustom pretty-speedbar-tags '("\uf02c" nil)
-  "Tags (plural) from FontAwesome used by pretty-tags."
+  "Tags (plural) from FontAwesome used by pretty-tags-plus and pretty-tags-minus."
   :group 'pretty-speedbar)
 
 (defcustom pretty-speedbar-info '("\uf05a" nil)
@@ -151,14 +143,6 @@ Defaults to off."
   "File from FontAwesome used by pretty-page-plus and pretty-page-minus."
   :group 'pretty-speedbar)
 
-(defcustom pretty-speedbar-plus '("\uf0fe" nil)
-  "Plus-square from FontAwesome used by pretty-plus."
-  :group 'pretty-speedbar)
-
-(defcustom pretty-speedbar-minus '("\uf146" nil)
-  "Minus-square from FontAwesome used by pretty-minus."
-  :group 'pretty-speedbar)
-
 (defcustom pretty-speedbar-check '("\uf00c")
   "Check from FontAwesome used by pretty-check."
   :group 'pretty-speedbar)
@@ -170,17 +154,16 @@ Defaults to off."
     (set-buffer-multibyte nil)
     (svg-print this-svg))))
 
-(defun pretty-speedbar-check-svg ()
-  "Create a checkmark smaller than the normal icons."
-  (setq this-name "pretty-check")
-  (setq this-icon (car pretty-speedbar-check))
+(defun pretty-speedbar-about-svg (this-name this-list)
+  "Create smaller informative icons, including checkmarks and locks to the right of file."
+  (setq this-icon (car this-list))
   (setq this-size (* 0.7 pretty-speedbar-icon-size))
 
   (let ((this-svg (svg-create this-size this-size :viewBox "0 0 512 512")))
   (svg-text this-svg this-icon
 	    :font-size "470px" ;;; changed from 507
-	    :stroke pretty-speedbar-check-stroke
-	    :fill pretty-speedbar-check-fill
+	    :stroke pretty-speedbar-about-stroke
+	    :fill pretty-speedbar-about-fill
 	    :font-family  (format "\'%s\'" pretty-speedbar-font)
 	    :x "50%"
 	    :y "450" ;;; changed from 90%
@@ -200,12 +183,12 @@ Defaults to off."
   (setq pretty-speedbar-icon-width (/ pretty-speedbar-icon-size 0.8643))
 
   (if is-folder
-    (setq this-pretty-icon-fill pretty-speedbar-icon-folder-fill)
-    (setq this-pretty-icon-fill pretty-speedbar-icon-fill))
-
-    (if is-folder
-      (setq this-pretty-icon-stroke pretty-speedbar-icon-folder-stroke)
-      (setq this-pretty-icon-stroke pretty-speedbar-icon-stroke)) 
+      (progn
+	(setq this-pretty-icon-fill pretty-speedbar-icon-folder-fill)
+	(setq this-pretty-icon-stroke pretty-speedbar-icon-folder-stroke))
+    (progn
+                      (setq this-pretty-icon-fill pretty-speedbar-icon-fill)
+                      (setq this-pretty-icon-stroke pretty-speedbar-icon-stroke)))
   
  (let ((this-svg (svg-create pretty-speedbar-icon-width  pretty-speedbar-icon-size :viewBox "0 0 512 512")))
   (svg-text this-svg this-icon
@@ -260,13 +243,13 @@ Defaults to off."
   (interactive)
   (pretty-speedbar-make-dir)
   (pretty-speedbar-blank-svg)
-  (pretty-speedbar-check-svg)
-
-  (pretty-speedbar-svg "pretty-lock" pretty-speedbar-lock)
-  (pretty-speedbar-svg "pretty-chevron-down" pretty-speedbar-chevron-down)
-  (pretty-speedbar-svg "pretty-chevron-right" pretty-speedbar-chevron-right)
+  ;;; Create informative icons to the right of the file.
+  (pretty-speedbar-about-svg "pretty-check" pretty-speedbar-check)
+  (pretty-speedbar-about-svg "pretty-lock" pretty-speedbar-lock)
+  ;;; Create file tree icons.
   (pretty-speedbar-svg "pretty-tag" pretty-speedbar-tag)
-  (pretty-speedbar-svg "pretty-tags" pretty-speedbar-tags)
+  (pretty-speedbar-svg "pretty-tags-plus" pretty-speedbar-tags "plus")
+   (pretty-speedbar-svg "pretty-tags-minus" pretty-speedbar-tags "minus")
   (pretty-speedbar-svg "pretty-info" pretty-speedbar-info)
   (pretty-speedbar-svg "pretty-mail" pretty-speedbar-mail)
   (pretty-speedbar-svg "pretty-book" pretty-speedbar-book)
@@ -276,11 +259,9 @@ Defaults to off."
   (pretty-speedbar-svg "pretty-page-minus" pretty-speedbar-blank-page "minus")
   (pretty-speedbar-svg "pretty-folder" pretty-speedbar-folder)
   (pretty-speedbar-svg "pretty-folder-open" pretty-speedbar-folder-open)
-  (pretty-speedbar-svg "pretty-page" pretty-speedbar-page)
-  (pretty-speedbar-svg "pretty-plus" pretty-speedbar-plus)
-  (pretty-speedbar-svg "pretty-minus" pretty-speedbar-minus))
+  (pretty-speedbar-svg "pretty-page" pretty-speedbar-page))
 
-;;; should be toggled by the setq
+;;; Toggle the pretty-check icon.
 (defmacro pretty-speedbar-vc-icon (this-file)
   "A blank svg image."
   `(defezimage pretty-check ((:type svg :file ,(expand-file-name (format "%s.svg" this-file) pretty-speedbar-icons-dir) :ascent center)) "Replacement for ezimage-checkout, which is described as files checked out of a vc with toggle."))
@@ -292,7 +273,7 @@ Defaults to off."
       (pretty-speedbar-vc-icon "pretty-check")
       (pretty-speedbar-vc-icon "blank")))
 
-  (pretty-speedbar-toggle-vc)
+(pretty-speedbar-toggle-vc)
 
 ;;; manually using pretty-page works. Now change it to variables.
 
@@ -301,30 +282,51 @@ Defaults to off."
   `(defezimage ,(intern (format "%s" this-name)) ((:type svg :file ,(expand-file-name (format "%s.svg" this-name) pretty-speedbar-icons-dir) :ascent center)) "Documentation string replace.")
   )
 
-;; passing the name manually works. Pulling it from the list does not.
+;; Manually add the correct documentation for each ezimage. Passing it to the defmacro itself causes compilation errors.
 (pretty-speedbar-ezimage "pretty-lock")
 (put 'pretty-lock 'function-documentation "Replacement for ezimage-lock, which is described as a read only or private.")
-(pretty-speedbar-ezimage "pretty-chevron-down")
-(pretty-speedbar-ezimage "pretty-chevron-right")
+
 (pretty-speedbar-ezimage "pretty-tag")
-(pretty-speedbar-ezimage "pretty-tags")
+(put 'pretty-tag 'function-documentation "Replacement for ezimage-tag, which is the primary tag icon used for the table of contents.")
+
+(pretty-speedbar-ezimage "pretty-tags-plus")
+(put 'pretty-tags-plus 'function-documentation "Replacement for ezimage-tag-gt, which is described as closed tags.")
+
+(pretty-speedbar-ezimage "pretty-tags-minus")
+(put 'pretty-tags-minus 'function-documentation "Replacement for ezimage-tag-v, which is described as open tags.")
+
 (pretty-speedbar-ezimage "pretty-info")
+(put 'pretty-info 'function-documentation "Replacement for ezimage-info.")
+
 (pretty-speedbar-ezimage "pretty-lock")
+(put 'pretty-lock 'function-documentation "Replacement for ezimage-lock, which is used for read only and private files and directories containing some of these files.")
+
 (pretty-speedbar-ezimage "pretty-mail")
+(put 'pretty-mail 'function-documentation "Replacement for ezimage-mail.")
+
 (pretty-speedbar-ezimage "pretty-book")
+(put 'pretty-book 'function-documentation "Replacement for ezimage-document-tag, which is described as documentation available.")
+
 (pretty-speedbar-ezimage "pretty-box-closed")
+(put 'pretty-box-closed 'function-documentation "Replacement for ezimage-box-plus, which refers to an expandable container used by the table of contents.")
+
 (pretty-speedbar-ezimage "pretty-box-open")
+(put 'pretty-box-open 'function-documentation "Replacement for ezimage-box-minus, which refers to an open box.")
+
 (pretty-speedbar-ezimage "pretty-folder")
+(put 'pretty-folder 'function-documentation "Replacement for ezimage-directory-plus and ezimage-directory, which both refer to closed directories.")
+
 (pretty-speedbar-ezimage "pretty-folder-open")
+(put 'pretty-folder-open 'function-documentation "Replacement for ezimage-directory-minus, which is described as an open directory.")
 
 (pretty-speedbar-ezimage "pretty-page")
 (put 'pretty-page 'function-documentation "Replacement for ezimage-page, which is described as file with nothing interesting in it.")
 
 (pretty-speedbar-ezimage "pretty-page-plus")
-(pretty-speedbar-ezimage "pretty-page-minus")
+(put 'pretty-page-plus 'function-documentation "Replacement for ezimage-page-plus, which is used by files with a table of contents speedbar can see.")
 
-(pretty-speedbar-ezimage "pretty-plus")
-(pretty-speedbar-ezimage "pretty-minus")
+(pretty-speedbar-ezimage "pretty-page-minus")
+(put 'pretty-page-minus 'function-documentation "Replacement for ezimage-plus-minus, which is to contract files with a table of contents speedbar can see.")
 
 ;;;###autoload
 (setq speedbar-expand-image-button-alist
@@ -339,18 +341,15 @@ Defaults to off."
     ("{-}" . pretty-box-open)
     ("<M>" . pretty-mail)
     ("<d>" . pretty-book)
-    ("<i>" . ezimage-info-tag)
-    (" =>" . ezimage-tag)
-    (" +>" . ezimage-tag-gt)
-    (" ->" . ezimage-tag-v)
-    (">"   . ezimage-tag)
-    ("@"   . ezimage-tag-type)
-    ("  @" . ezimage-tag-type)
+    ("<i>" . pretty-info)
+    (" =>" . pretty-tag)
+    (" +>" . pretty-tags-plus)
+    (" ->" . pretty-tags-minus)
+    (">"   . pretty-tag)
+    ("@"   . pretty-tag)
+    ("  @" . pretty-tag)
     ("*"   . pretty-check)
-    ("#"   . ezimage-object)
-    ("!"   . ezimage-object-out-of-date)
-    ("//"  . ezimage-label)
-    ("%"   . ezimage-lock)
+    ("%"   . pretty-lock)
     ))
 
 
